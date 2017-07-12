2e99f045a1fbb0a57e4fc4a3697620ed6501a830
曹汉超 <627203960@qq.com>
2017-07-12 16:40:04 +0800

    变更

--- SpriteKitSimpleGame/AppDelegate.m
+++ SpriteKitSimpleGame/AppDelegate.m
@@ -16,6 +16,8 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    //变更
+    
     // Override point for customization after application launch.
     return YES;
 }
